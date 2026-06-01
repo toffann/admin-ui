5dda7f048953725cf14251d6b4bcca51cd6f8ac0
Taufan Afandi <tautauffn@gmail.com>
2026-06-02 06:39:41 +0700

    React Icons (Library and Custom)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
 import ErrorPage from "./pages/error";
 import DashboardPage from "./pages/dashboard";
+import BalancePage from "./pages/balance";
 import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
 function App() {
@@ -19,6 +20,10 @@ function App() {
     {
       path: "/register",
       element: <SignUpPage />,
+    },
+    {
+      path: "/balance",
+      element: <BalancePage />,
     },
   ]);
 
